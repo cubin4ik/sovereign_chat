90ec68a1fd10292806b6bd168a049843e1556e2c
Alejandro Suarez <cubin4ik@mail.ru>
2022-01-25 04:41:22 +0300

    __name__ statement added, junky variable my_server removed

--- server/server.py
+++ server/server.py
@@ -4,5 +4,6 @@
 # DELETE "server." as a reference (it is there to run both client and server from one project directory)
 from server.connection import Connection
 
-print(f"SERVER LAUNCHED at address: {Connection.IP}:{Connection.PORT}")
-my_server = Connection("server")
+if __name__ == '__main__':
+    print(f"LAUNCHING SERVER at address: {Connection.IP}:{Connection.PORT}")
+    Connection('server')
