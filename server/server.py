2d0efce364e17a409443516649dc4cc46dd0ac0c
suarez_work_win <cubin4ik@mail.ru>
2022-01-29 00:15:29 +0300

    changed import order, removed comnnet regarding standard lib import

--- server/server.py
+++ server/server.py
@@ -1,11 +1,8 @@
 """Testing server class"""
 
-# standard libraries
-import logging
-
-# local files
-from server.connection import Connection
 # DELETE "server." as a reference (it is there to run both client and server from one project directory)
+from server.connection import Connection
+import logging
 
 logging.basicConfig(level=logging.WARNING, format='[%(levelname)s] %(asctime)s - %(message)s')
 
